82d60676e86ed68f6dc3dd46736ba52d493b1983
Miroslav-Miro <miroslavmazalov@gmail.com>
2022-03-14 21:27:10 +0200

    Test!!! Add text in "ConsoleApp"

--- DigitGallery/DigitGallery.ConsoleApp/Program.cs
+++ DigitGallery/DigitGallery.ConsoleApp/Program.cs
@@ -6,7 +6,7 @@ namespace DigitGallery.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! Heyyy... Nadya:)");
         }
     }
 }
