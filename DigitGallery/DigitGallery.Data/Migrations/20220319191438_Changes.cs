952fc1a69caa6f8bfaef64c5612f67d49c639c3b
Miroslav-Miro <miroslavmazalov@gmail.com>
2022-03-19 22:44:41 +0200

    Add

---
+++ DigitGallery/DigitGallery.Data/Migrations/20220319191438_Changes.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DigitGallery.Data.Migrations
+{
+    public partial class Changes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Artists");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Artists",
+                type: "nvarchar(12)",
+                maxLength: 12,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
